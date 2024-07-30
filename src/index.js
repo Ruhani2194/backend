4670a604f244149ee3527decdd16ea3dc67a5f01
Ruhani <ruhani2194.be22@chitkara.edu.in>
2024-07-30 09:00:21 +0530

    add user and video model

---
+++ src/index.js
@@ -0,0 +1,41 @@
+// require('dotenv').config({path:'./env'});//code ki consistency ko kharb krta h
+// import mongoose from "mongoose";
+// import { DB_NAME } from "./constants";
+// import express from"express"
+
+import dotenv from "dotenv" 
+import connectDB from "./db/index.js";
+
+//database always in other continent
+// 1. approach
+
+// ;(async()=>{
+//     try{
+// await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`)
+// app.on("error",(error)=>{
+//     console.log("ERROR",error);
+//     throw error
+// })
+
+// app.listen(process.env.PORT,()=>{
+//     console.log(`App is Listenining on port ${process.env.PORT}`);
+// })
+//     } catch(error){
+//         console.log('ERROR',error)
+//     }
+// })()
+
+// approch2
+// import {app} from './app.js '
+dotenv.config({
+    path:'./env'
+})
+connectDB()
+.then(()=>{
+    app.listen(process.env.PORT||8000,()=>{
+        console.log(`Server is running at port:${process.env.PORT}`);
+    })
+})
+.catch((err)=>{
+console.log("Mongo db connection failed!!!",err);
+});
